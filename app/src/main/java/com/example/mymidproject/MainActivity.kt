248380ed7fa5ec1a0ee89b02933081d995c03cbd
Daniel39177937 <39177937wasde@gmail.com>
2022-04-18 21:08:15 +0800

    main picture v1

---
+++ app/src/main/java/com/example/mymidproject/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.mymidproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.core.view.ViewCompat.setBackground
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
